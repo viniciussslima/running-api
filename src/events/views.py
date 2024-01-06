00440356d785a4b2b57e713fa887f031823916b3
Vinicius Santos Silva de Lima <viniciussslima999@gmail.com>
2024-01-06 17:26:34 -0300

    feat: :sparkles: added a distances list action

--- src/events/views.py
+++ src/events/views.py
@@ -1,6 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
 
 from .models import Event
 from .serializers import EventListSerializer, EventSerializer
@@ -21,3 +23,9 @@ class EventViewSet(viewsets.ModelViewSet):
             return EventListSerializer
 
         return super().get_serializer_class()
+
+    @action(detail=False, methods=["GET"])
+    def distances(self, request):
+        distances = Event.objects.values_list("distance", flat=True).distinct().order_by("distance")
+
+        return Response({"distances": distances})
